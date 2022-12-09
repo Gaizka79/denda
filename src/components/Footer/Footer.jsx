10b7d7c76cdb0fccbb445ec1547964bd334b94d0
Gaizka79 <agaizka79@gmail.com>
2022-12-09 22:51:51 +0100

    bug fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer () {
   return (
     <footer>
-      <a href="https://github.com/Gaizka79/denda.git">Developed by Gaizka</a>
+      <a href="https://github.com/Gaizka79/denda.git" target="_blank">Developed by Gaizka</a>
     </footer>
   )
 }
